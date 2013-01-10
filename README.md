f3e6587225b41832f7e471230b732459d8be73e8
maodouzi <maodouzi@gmail.com>
2013-01-10 23:16:01 +0800

    Customizing admin change lists
    http://localhost:8080/admin/books/author/

--- README.md
+++ README.md
@@ -693,6 +693,10 @@ theDjangoBook2.0
 
 		http://localhost:8080/admin/books/author/add/
 		
-		first_name => First name:
-		last_name => Last name:
-		e-mail => E-mail: 
+			first_name => First name:
+			last_name => Last name:
+			e-mail => E-mail:
+
+	1.	Customizing admin change lists
+
+		http://localhost:8080/admin/books/author/
--- mysite/mysite/books/admin.py
+++ mysite/mysite/books/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from mysite.books.models import Publisher, Author, Book
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
